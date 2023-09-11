c4e934998f9b6829af43ab5061fa3fb556bc6012
Александр Горбунов <Tyuiu.GorbunovAA@outlook.com>
2023-09-11 14:15:11 +0500

    Task4

---
+++ Tyuiu.GorbunovAA.Sprint0.Task4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorbunovAA.Sprint0.Task4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorbunovAA.Sprint0.Task4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d01b7d53-7e49-4a4d-978a-3254d69fdf62")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GorbunovAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorbunovAA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.GorbunovAA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(3, 3));
+            Console.WriteLine(DataService.Substraction(9, 3));
+            Console.WriteLine(DataService.Multiplication(2, 3));
+            Console.WriteLine(DataService.Division(12, 2));
+            Console.ReadKey();
+        }
+    }
+}
